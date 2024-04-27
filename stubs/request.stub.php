ee1640b3badbd0729d35135cf3a2de5dd9efa355
Vic GUTT <guttvictor@yahoo.fr>
2024-04-27 13:21:16 +0200

    feat: removed depricated validation rule contract form request

--- stubs/request.stub.php
+++ stubs/request.stub.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace ___LaravelStubs___namespace;
 
-use Illuminate\Contracts\Validation\Rule;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Contracts\Validation\ValidationRule;
 
@@ -21,7 +20,7 @@ final class ___LaravelStubs___class extends FormRequest
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, Rule|ValidationRule|array|string>
+     * @return array<string, ValidationRule|array|string>
      */
     public function rules(): array
     {
