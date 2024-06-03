5762d1deebe24b06363ee2ec356bff2525b0669d
Vic GUTT <guttvictor@yahoo.fr>
2024-06-03 08:13:31 +0200

    feat: added "Prohibitable" trait to console stub

--- stubs/console.stub.php
+++ stubs/console.stub.php
@@ -5,9 +5,12 @@ declare(strict_types=1);
 namespace ___LaravelStubs___namespace;
 
 use Illuminate\Console\Command;
+use Illuminate\Console\Prohibitable;
 
 final class ___LaravelStubs___class extends Command
 {
+    use Prohibitable;
+
     /**
      * The name and signature of the console command.
      */
@@ -23,6 +26,10 @@ final class ___LaravelStubs___class extends Command
      */
     public function handle(): int
     {
+        if ($this->isProhibited()) {
+            return self::FAILURE;
+        }
+
         return self::SUCCESS;
     }
 }
